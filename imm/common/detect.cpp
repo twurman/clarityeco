f98124917d5d159fd2df6b26ca7fc7fd74b2ba01
Tim W <twurman@umich.edu>
2015-06-25 14:10:33 -0400

    fixing filepath in detect

--- imm/common/detect.cpp
+++ imm/common/detect.cpp
@@ -334,7 +334,7 @@ void build_model(DescriptorMatcher *matcher, vector<string> *trainImgs){
 	DescriptorExtractor *extractor = new SurfDescriptorExtractor();
 	
   // Generate desc
-  string db = fs::current_path().parent_path().string() + "common/matching/landmarks/db";
+  string db = fs::current_path().parent_path().string() + "/common/matching/landmarks/db";
   cerr << db << endl;
   assert(db == "/home/twurman/clarityeco/imm/common/matching/landmarks/db");
 	fs::path p = fs::system_complete(db);
