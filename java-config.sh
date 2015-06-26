07a14ab2416fe334757764e32b5f2e3537a02b39
Ben Toll <tollben@umich.edu>
2015-06-25 22:46:59 -0400

    updated asr and qa build systems

---
+++ java-config.sh
@@ -0,0 +1,2 @@
+# Set classpaths for java programs in clarityeco
+export thrift=~/thrift-0.9.2
--- qa/sirius/compile-qa-server-thrift.sh
+++ qa/sirius/compile-qa-server-thrift.sh
@@ -4,13 +4,20 @@
 # Now javac knows to look for the class info.ephyra.OpenEphyra in the directory info/ephyra/
 # However, javac still needs the classpath to the package.
 
+# Rather than forking a subshell, execute all commands
+# in java-config.sh in the current shell.
+source ../../java-config.sh
+
+echo "Using thrift directory $thrift..."
+
 # Add thrift libraries to class path
-export CLASSPATH=~/thrift-0.9.2/lib/java/build/libthrift-0.9.2.jar:~/thrift-0.9.2/lib/java/build/lib/slf4j-api-1.5.8.jar:~/thrift-0.9.2/lib/java/build/lib/slf4j-log4j12-1.5.8.jar:~/thrift-0.9.2/lib/java/build/lib/log4j-1.2.14.jar
+export JAVA_CLASS_PATH=$thrift/lib/java/build/libthrift-0.9.2.jar:$thrift/lib/java/build/lib/slf4j-api-1.5.8.jar:$thrift/lib/java/build/lib/slf4j-log4j12-1.5.8.jar:$thrift/lib/java/build/lib/log4j-1.2.14.jar
 
 # Add command center to class path
-export CLASSPATH=$CLASSPATH:~/sirius/sirius-application/command-center/gen-java
+export JAVA_CLASS_PATH=$JAVA_CLASS_PATH:../../command-center/gen-java
 
-# Add open ephyra libraries to class path
-export CLASSPATH=$CLASSPATH:../common/question-answer/bin
+# Add OpenEphyra libraries to class path
+export JAVA_CLASS_PATH=$JAVA_CLASS_PATH:../common/question-answer/bin
 
-javac QADaemon.java QAServiceHandler.java gen-java/qastubs/QAService.java
+# Use cp flag to avoid cluttering up the CLASSPATH environment variable
+javac -cp $JAVA_CLASS_PATH QADaemon.java QAServiceHandler.java gen-java/qastubs/QAService.java
