9312d81b4704aeb62e68c2045eb615b8ceeb7b0a
Ben Toll <tollben@umich.edu>
2015-06-29 11:36:01 -0400

    minor improvements to template/compile-qa.sh

--- qa/template/compile-qa.sh
+++ qa/template/compile-qa.sh
@@ -4,23 +4,36 @@
 # Now javac knows to look for the class info.ephyra.OpenEphyra in the directory info/ephyra/
 # However, javac still needs the classpath to the package.
 
+printdivision()
+{
+	echo -e "\n"
+	for i in `seq 1 70`; do
+		echo -n "#";
+	done
+	echo -e "\n"
+}
+
 # Generate thrift files
-echo -e "Compiling thrift source code...\n"
+echo -e "./compile-qa.sh: `pwd`"
+echo -e "./compile-qa.sh: Compiling thrift source code..."
 thrift --gen java --gen cpp qaservice.thrift
+printdivision
 
 # Compile client
-echo -e "Making QA client...\n"
+echo -e "./compile-qa.sh: Making QA client..."
 make
+printdivision
 
 # Compile server
 # 'source' command: Rather than forking a subshell, execute all commands
 # in the current shell.
 cd ../common
 	./compile-openephyra.sh
+	printdivision
 	source ./qa-compile-config.sh
+	printdivision
 cd ../template
-echo -e "Using thrift directory $thrift...\n"
 
 # Use cp flag to avoid cluttering up the CLASSPATH environment variable
-echo -e "Compiling QA server...\n"
+echo -e "javac -cp $JAVA_CLASS_PATH QADaemon.java QAServiceHandler.java gen-java/qastubs/QAService.java\n\n"
 javac -cp $JAVA_CLASS_PATH QADaemon.java QAServiceHandler.java gen-java/qastubs/QAService.java
